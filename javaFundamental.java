db979fd1331baa9c0b1174318dd2806cc99052c2
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-13 23:53:55 +0600

    fundamental

--- javaFundamental.java
+++ javaFundamental.java
@@ -1450,7 +1450,17 @@ public class javaFundamental {
 
 
 
-
+for(int i = 1; i <= 100; i++){
+    if (i % 7 ==0 && i % 9 == 0){
+        System.out.println("Merge");
+    }else if (i % 7 == 0 ){
+        System.out.println("Seven");
+    }else if (i % 9 == 0){
+        System.out.println("Nine");
+    }else {
+        System.out.println(i);
+    }
+}
 
 
 
