7decb18454717f67091e524fea6d937cb1bf486a
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-26 11:37:02 +0600

    Fundamental

--- Test.java
+++ Test.java
@@ -15,19 +15,60 @@
 
 
 
-public class Test {
-    public static void main(String[] args){
-        for (int i = 1; i <= 100; i++){
-            if (i % 7 == 0 && i % 9 == 0){
-                System.out.println("Divisible by both 7 and 9: " + i);
-            } else if (i % 7 == 0){
-                System.out.println("Divisible by 7: " + i);
-            } else if (i % 9 == 0){
-                System.out.println("Divisible by 9: " + i);
-            }else{
-                System.out.println(i);
-            }
+// public class Test {
+//     public static void main(String[] args){
+//         for (int i = 1; i <= 100; i++){
+//             if (i % 7 == 0 && i % 9 == 0){
+//                 System.out.println("Divisible by both 7 and 9: " + i);
+//             } else if (i % 7 == 0){
+//                 System.out.println("Divisible by 7: " + i);
+//             } else if (i % 9 == 0){
+//                 System.out.println("Divisible by 9: " + i);
+//             }else{
+//                 System.out.println(i);
+//             }
+//         }
+//     }
+// }
+
+
+
+
+// // Reverse ArrayList without Collections.reverse()
+
+// import java.util.ArrayList;
+
+// public class Test {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         for (int i = 1; i <= 5; i++) list.add(i);
+
+//         for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
+//             int temp = list.get(i);
+//             list.set(i, list.get(j));
+//             list.set(j, temp);
+//         }
+
+//         System.out.println("Reversed List: " + list);
+//     }
+// }
+
+
+import java.util.ArrayList;
+
+public class Test{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) list.add(i);
+        System.out.println("Original List: " + list);
+
+        for (int i =  0, j = list.size() - 1; i < j; i++, j--) {
+            int temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
         }
+        System.out.println("Reversed List: " + list);
+        
+        // System.out.println("Original List: " + list.size()  );
     }
 }
-
