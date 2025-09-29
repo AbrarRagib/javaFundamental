4da593db0652d8840ce82f7beb6d70c5643b2f19
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-29 16:19:45 +0600

    Fundamental

--- Test.java
+++ Test.java
@@ -62,7 +62,7 @@ public class Test{
         for (int i = 1; i <= 5; i++) list.add(i);
         System.out.println("Original List: " + list);
 
-        for (int i =  0, j = list.size() - 1; i < j; i++, j--) {
+        for (int i =  0, j = list.size() - 1; i < j; i++, j++) {
             int temp = list.get(i);
             list.set(i, list.get(j));
             list.set(j, temp);
